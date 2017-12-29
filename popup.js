1abfe21aa2534448550416f3b979a2bcf1d854c8
Steven Antonio <steven@stevenanton.io>
2017-12-29 03:57:32 -0700

    commit 12/29/17 need to dry and style

--- popup.js
+++ popup.js
@@ -15,9 +15,9 @@ function renderURL(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
-
-function aRecordLookup(domain){
-  var a = domain;
+function cleanURL(dirtyURL){
+  var a = dirtyURL;
+  a = a.split('/')[2];
   a = a
     .replace('https', '')
     .replace('http', '')
@@ -25,22 +25,40 @@ function aRecordLookup(domain){
     .replace('://', '')
     .replace(/\/$/, '')
   console.log(a);
+  return a
+}
+
+// function lookupDNS(_prefix, _domain){
+//   var [lookupPrefix, lookupDomain] = [_prefix, _domain];
+//   console.log("THe lookip is: "+lookupPrefix+lookupDomain);
+//   axios.get(lookupPrefix+lookupDomain)
+//   .then(function (response){
+//     var jsonResponse = response.data;
+//     console.log(jsonResponse);
+//     return jsonResponse;
+//   })
+//   .catch(function (error){
+//     console.log(error);
+//   });
+// }
+
+function aRecordLookup(domain){
+  var a = domain;
   axios.get('https://dns-api.org/A/'+a)
   .then(function (response){
-    // console.log(response);
     var jsonResponse = response.data;
-    // console.log(jsonResponse);
     var arecords = document.getElementById("a-records");
     for(var i = 0; i < jsonResponse.length; i++){
-      stringResponse = JSON.stringify(jsonResponse[i].value);
-      console.log(stringResponse);
+      stringResponseName = JSON.stringify(jsonResponse[i].name);
+      stringResponseValue = JSON.stringify(jsonResponse[i].value);
+      stringResponseTTL = JSON.stringify(jsonResponse[i].ttl);
       var node = document.createElement("li") // Create <li> node
-      var textNode = document.createTextNode(stringResponse); // Create text node
+      var textNode = document.createTextNode(
+          "Name: " + stringResponseName + " | " +
+          "Value: " + stringResponseValue + " | " +
+          "TTL: " + stringResponseTTL); // Create text node
       node.appendChild(textNode); // Append the text to the <li>
       arecords.appendChild(node);
-      // arecord.innerHTML += stringResponse;
-      // var arecord = document.createTextNode(stringResponse);
-      // document.getElementById("a-records").innerHTML = stringResponse;
     }
   })
   .catch(function (error){
@@ -48,10 +66,148 @@ function aRecordLookup(domain){
   });
 }
 
+// MXRECORDS //////////////////////////////
+///////////////////////////////////////
+function mxRecordLookup(domain){
+  var a = domain;
+  axios.get('https://dns-api.org/MX/'+a)
+  .then(function (response){
+    var jsonResponse = response.data;
+    var mxrecords = document.getElementById("mx-records");
+    for(var i = 0; i < jsonResponse.length; i++){
+      stringResponseName = JSON.stringify(jsonResponse[i].name);
+      stringResponseValue = JSON.stringify(jsonResponse[i].value);
+      stringResponseTTL = JSON.stringify(jsonResponse[i].ttl);
+      var node = document.createElement("li") // Create <li> node
+      var textNode = document.createTextNode(
+          "Name: " + stringResponseName + " | " +
+          "Value: " + stringResponseValue + " | " +
+          "TTL: " + stringResponseTTL); // Create text node
+      node.appendChild(textNode); // Append the text to the <li>
+      mxrecords.appendChild(node);
+    }
+  })
+  .catch(function (error){
+    console.log(error);
+  });
+}
+// CNAME //////////////////////
+///////////////////////////////
+function cnameRecordLookup(domain){
+  var a = domain;
+  axios.get('https://dns-api.org/CNAME/'+a)
+  .then(function (response){
+    var jsonResponse = response.data;
+    var cnamerecords = document.getElementById("cname-records");
+    for(var i = 0; i < jsonResponse.length; i++){
+      stringResponseName = JSON.stringify(jsonResponse[i].name);
+      stringResponseValue = JSON.stringify(jsonResponse[i].value);
+      stringResponseTTL = JSON.stringify(jsonResponse[i].ttl);
+      var node = document.createElement("li") // Create <li> node
+      var textNode = document.createTextNode(
+          "Name: " + stringResponseName + " | " +
+          "Value: " + stringResponseValue + " | " +
+          "TTL: " + stringResponseTTL); // Create text node
+      node.appendChild(textNode); // Append the text to the <li>
+      cnamerecords.appendChild(node);
+    }
+  })
+  .catch(function (error){
+    console.log(error);
+  });
+}
+
+// TXT /////////////////////////////
+////////////////////////////////////
+function txtRecordLookup(domain){
+  var a = domain;
+  axios.get('https://dns-api.org/TXT/'+a)
+  .then(function (response){
+    var jsonResponse = response.data;
+    var txtrecords = document.getElementById("txt-records");
+    for(var i = 0; i < jsonResponse.length; i++){
+      stringResponseName = JSON.stringify(jsonResponse[i].name);
+      stringResponseValue = JSON.stringify(jsonResponse[i].value);
+      stringResponseTTL = JSON.stringify(jsonResponse[i].ttl);
+      var node = document.createElement("li") // Create <li> node
+      var textNode = document.createTextNode(
+          "Name: " + stringResponseName + " | " +
+          "Value: " + stringResponseValue + " | " +
+          "TTL: " + stringResponseTTL); // Create text node
+      node.appendChild(textNode); // Append the text to the <li>
+      txtrecords.appendChild(node);
+    }
+  })
+  .catch(function (error){
+    console.log(error);
+  });
+}
+
+// SOA /////////////////////////////
+////////////////////////////////////
+function soaRecordLookup(domain){
+  var a = domain;
+  axios.get('https://dns-api.org/SOA/'+a)
+  .then(function (response){
+    // console.log(response);
+    var jsonResponse = response.data;
+    // console.log(jsonResponse);
+    var soarecords = document.getElementById("soa-records");
+    for(var i = 0; i < jsonResponse.length; i++){
+      stringResponseName = JSON.stringify(jsonResponse[i].name);
+      stringResponseValue = JSON.stringify(jsonResponse[i].value);
+      stringResponseTTL = JSON.stringify(jsonResponse[i].ttl);
+      var node = document.createElement("li") // Create <li> node
+      var textNode = document.createTextNode(
+          "Name: " + stringResponseName + " | " +
+          "Value: " + stringResponseValue + " | " +
+          "TTL: " + stringResponseTTL); // Create text node
+      node.appendChild(textNode); // Append the text to the <li>
+      soarecords.appendChild(node);
+    }
+  })
+  .catch(function (error){
+    console.log(error);
+  });
+}
+
+// NS /////////////////////////////
+////////////////////////////////////
+function nsRecordLookup(domain){
+  var a = domain;
+  axios.get('https://dns-api.org/NS/'+a)
+  .then(function (response){
+    var jsonResponse = response.data;
+    var nsrecords = document.getElementById("ns-records");
+    for(var i = 0; i < jsonResponse.length; i++){
+      stringResponse = JSON.stringify(jsonResponse[i].value);
+      var node = document.createElement("li") // Create <li> node
+      var textNode = document.createTextNode(stringResponse); // Create text node
+      node.appendChild(textNode); // Append the text to the <li>
+      nsrecords.appendChild(node);
+    }
+  })
+  .catch(function (error){
+    console.log(error);
+  });
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("feedback-button").addEventListener("click", function(){
+    alert("Contact form coming soon, email me at steven@stevenanton.io until then")
+  });
   getCurrentTabUrl(function(url) {
-    renderURL(url);
-    aRecordLookup(url);
+    var _cleanURL = cleanURL(url);
+    console.log("dirty url: " + url);
+    console.log("clean url: " + _cleanURL);
+    renderURL(_cleanURL);
+    aRecordLookup(_cleanURL);
+    mxRecordLookup(_cleanURL);
+    cnameRecordLookup(_cleanURL);
+    txtRecordLookup(_cleanURL);
+    soaRecordLookup(_cleanURL);
+    nsRecordLookup(_cleanURL);
   });
 });
 
